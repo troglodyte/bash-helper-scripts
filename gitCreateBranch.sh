2e2525b2510f02bcafbf7b2430f457526e6518a6
Michael T Harris <throwingfish@gmail.com>
2017-12-14 09:55:35 -0500

    Filtering out forward slashes

--- gitCreateBranch.sh
+++ gitCreateBranch.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+
 echo "Ticket number?"
 read ticket_number
+
 title=$@
 suffix=`echo $@ | awk '{print tolower($0)}'`
-# trim extra dashes
-suffix=${suffix//[\!,.-@?.]/}
+
+# Filter out unwanted strings 
+suffix=${suffix//[\!,.-@?.\/]/}
+
 # trim extra spaces
 suffix=`echo $suffix | tr -s " "`
+
 # replace spaces with dashes
-suffix=${suffix// /-}
+suffix=${suffix// /-} 
+
 ticket_number=`echo $ticket_number | awk '{print tolower($0)}'`
-echo "$ticket_number"
 branch_name="ticket-$ticket_number-mharris-$suffix"
+
 git checkout -b $branch_name

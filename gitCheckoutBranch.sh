b3db53ea38b3b1f2d0c523b5c8018f0ba7c48c4c
Michael T Harris <throwingfish@gmail.com>
2018-03-28 12:20:50 -0400

    Update gitCheckoutBranch.sh
    oops

--- gitCheckoutBranch.sh
+++ gitCheckoutBranch.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
-
-echo "Ticket number?"
-read ticket_number
-
-title=$@
-
-suffix=`echo $@ | awk '{print tolower($0)}'`
-
-# Filter out unwanted strings 
-suffix=${suffix//[\!,.-@?.\/]/}
-
-# trim extra spaces
-suffix=`echo $suffix | tr -s " "`
-
-# Extract substring
-suffix=${suffix:0:45}
-
-# Strip trailing space
-suffix="$(echo -e "${suffix}" | sed -e 's/[[:space:]]*$//')"
-
-# replace spaces with dashes
-suffix=${suffix// /-} 
-
-ticket_number=`echo $ticket_number | awk '{print tolower($0)}'`
-branch_name="ticket-$ticket_number-your_username_here-$suffix"
-
-git checkout -b $branch_name
+printf "Stashing current changes if any\n"
+git stash
+git fetch --all
+tag=${1?Needs ticket number}
+cur=`git branch --all | grep -m 1 $tag | sed "s/\*//g"`
+cur=`echo $cur | sed "s/remotes\///g" | sed "s/origin\///g"`
+printf "\n\n***********************\n"
+printf "Searching for #$tag -> $cur\n"
+printf "        ¯\_(ツ)_/¯\n"
+printf "***********************\n\n"
+git checkout $cur
+git pull origin $cur
